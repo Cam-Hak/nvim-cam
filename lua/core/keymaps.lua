c31bdccd3be9ecced539d507c71585e09ba1c4c5
Cameron Hakenson <camhakenson@gmail.com>
2024-12-15 09:12:07 -0500

    changed cursor

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,3 @@
-
 -- Set leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -7,7 +6,7 @@ local opts = { noremap = true, silent = true }
 
 -- easy esc
 vim.keymap.set('i', 'jk', '<Esc>', opts)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Resize with arrows
 vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
@@ -18,11 +17,11 @@ vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 -- Buffers
 vim.keymap.set('n', 'L', ':bnext<CR>', opts)
 vim.keymap.set('n', 'H', ':bprevious<CR>', opts)
-vim.keymap.set('n', 'D', ':Bdelete!<CR>',opts)
+vim.keymap.set('n', 'D', ':Bdelete!<CR>', opts)
 
 -- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
-vim.keymap.set('n', '<leader>s', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
+vim.keymap.set('n', '<leader>s', '<C-w>s', opts)      -- split window horizontally
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
@@ -35,4 +34,4 @@ vim.keymap.set('n', '<leader>l', ':wincmd l<CR>', opts)
 vim.keymap.set('v', 'p', '"_dP', opts)
 
 -- cursor
-vim.opt.guicursor = "n-v-c-i:block"
+-- vim.opt.guicursor = "n-v-c-i:block"
