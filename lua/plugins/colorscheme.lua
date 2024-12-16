570677bbc5e7710a62d90e1113c85424abb86579
Cameron Hakenson <camhakenson@gmail.com>
2024-12-16 10:55:14 -0500

    new color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -23,30 +23,34 @@
 --   end,
 -- }
 return {
-  'shaunsingh/nord.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Example config in lua
-    vim.g.nord_contrast = true
-    vim.g.nord_borders = false
-    vim.g.nord_disable_background = true
-    vim.g.nord_italic = false
-    vim.g.nord_uniform_diff_background = true
-    vim.g.nord_bold = false
-
-    -- Load the colorscheme
-    require('nord').set()
-
-    -- Toggle background transparency
-    local bg_transparent = true
-
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd [[colorscheme nord]]
-    end
-
-    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+  'Mofiqul/vscode.nvim',
+  init = function()
+    vim.cmd.colorscheme 'vscode'
   end,
+  -- 'shaunsingh/nord.nvim',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   -- Example config in lua
+  --   vim.g.nord_contrast = true
+  --   vim.g.nord_borders = false
+  --   vim.g.nord_disable_background = true
+  --   vim.g.nord_italic = false
+  --   vim.g.nord_uniform_diff_background = true
+  --   vim.g.nord_bold = false
+  --
+  --   -- Load the colorscheme
+  --   require('nord').set()
+  --
+  --   -- Toggle background transparency
+  --   local bg_transparent = true
+  --
+  --   local toggle_transparency = function()
+  --     bg_transparent = not bg_transparent
+  --     vim.g.nord_disable_background = bg_transparent
+  --     vim.cmd [[colorscheme nord]]
+  --   end
+  --
+  --   vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+  -- end,
 }
